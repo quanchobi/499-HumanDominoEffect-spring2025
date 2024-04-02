73012c92dbaf441a41e832170c03766a40772ca2
RLSwastaken <rileylsheridan@gmail.com>
2024-04-01 20:18:49 -0400

    Small audio fix for title screen, test for git push

--- Scenes/Title_Scene.gd
+++ Scenes/Title_Scene.gd
@@ -32,7 +32,7 @@ func handleChangeToMenuScene():
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
+	MusicController.playMusic("res://audio/background/quantum.ogg")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
