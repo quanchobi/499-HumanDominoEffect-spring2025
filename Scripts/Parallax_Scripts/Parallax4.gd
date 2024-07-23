f43fdfcb9df45baa5b5a0209e72cb2b886534d67
RLSwastaken <rileylsheridan@gmail.com>
2024-07-22 21:45:53 -0400

    Added test text in Parallax4

--- Scripts/Parallax_Scripts/Parallax4.gd
+++ Scripts/Parallax_Scripts/Parallax4.gd
@@ -9,24 +9,36 @@ signal pan_stopped
 # var a = 2
 # var b = "text"
 ### var speed = 100
-var speed = 1000
+var speed = 500
 var direction = Vector2(-1, 0)
 var is_transition_ready = false
+
+var narration_text = ["Test test test test test test test test test test test test test test"]
+var narration_count = 1
 
 onready var parallax = $ParallaxBackground
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	emit_signal("trigger_animation", "Screen_Unwipe") # Replace with function body.
+	
+	$Narration.text = narration_text[0]
+	$Narration/TextAnimationPlayer.play("Reveal", -1, 2)
 
 func _process(delta):
-	if parallax.scroll_offset.x >= -6100:
+	if parallax.scroll_offset.x >= -6125:
 		parallax.scroll_offset += direction * speed * delta
 	else:
 		emit_signal("pan_stopped")
 
 func _on_CavepanTimer_timeout():
 	emit_signal("trigger_animation", "Screen_Wipe") 
+	
+func _on_TextAnimationPlayer_animation_finished(anim_name: String) -> void:
+	if narration_count < len(narration_text):
+		$Narration.text = narration_text[narration_count]
+		$Narration/TextAnimationPlayer.play("Reveal")
+		narration_count += 1
 
 func _on_AnimationPlayer_animation_finished(anim_name: String) -> void:
 	if (anim_name == "Screen_Wipe"):
