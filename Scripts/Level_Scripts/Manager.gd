08b523336af59638e1e8fc0dbc5da98b5305cf46
Uran <dayouran.lim@gmail.com>
2024-12-09 22:41:22 -0500

    DominoWorldTutorial

--- Scripts/Level_Scripts/Manager.gd
+++ Scripts/Level_Scripts/Manager.gd
@@ -6,7 +6,11 @@ var current_level = null
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	current_level = load(ReferenceManager.get_reference(gamestate.first_level + ".tscn")).instance()
+	if gamestate.get_tutorial_mode():
+		current_level = load(ReferenceManager.get_reference(gamestate.first_level + "Tutorial.tscn")).instance()
+	else:
+		current_level = load(ReferenceManager.get_reference(gamestate.first_level + ".tscn")).instance()
+	
 	add_child(current_level)
 	
 func change_level(next_scene):
--- Scripts/Tutorial.gd
+++ Scripts/Tutorial.gd
@@ -1,11 +1,7 @@
-extends Control
+extends Node
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
 onready var TutorialContainer = $Tutorial_Container
 onready var LevelSelectContainer = $LevelSelect_Container
-#Hello
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -13,10 +9,18 @@ func _ready():
 	
 	# gamestate.gd signal event listeners
 	gamestate.connect("player_list_changed", self, "refresh_lobby")
+	#Enable tutorial mode flag
+	gamestate.tutorial_mode = true 
+	# If user press start without selecting level automatically goes to the first level
+	
+	# CS499 Fall 2024 Only DominoWorld have tutorial
+	handle_level("DominoWorld")
 
+# Function to start the game
 func _on_Start_pressed():
 	gamestate.begin_game()
-	
+
+# Load in the level selection menu
 func _on_Select_pressed():
 	# Load the level select menu with transition
 	change_menu_smoothly(TutorialContainer, LevelSelectContainer)
@@ -36,21 +40,25 @@ func change_menu_smoothly(prev, target):
 func handle_level(level):
 	gamestate.first_level = level
 
-	for top in range(10):
-		for bottom in range(top + 1):
-			gamestate.dominos.append([bottom, top])
+	#for top in range(10):
+	#	for bottom in range(top + 1):
+	#		gamestate.dominos.append([bottom, top])
 
-	randomize()
-	gamestate.random_seed = randi() % 10000000
-	seed(gamestate.random_seed)
+	#randomize()
+	#gamestate.random_seed = randi() % 10000000
+	#seed(gamestate.random_seed)
 
-	gamestate.dominos.shuffle()
+	#gamestate.dominos.shuffle()
 	
 	# Change menu to waiting room
 	change_menu_smoothly(LevelSelectContainer, TutorialContainer)
 	
+	# Host a game with a player name Tutorial
 	var player_name = "Tutorial"
 	gamestate.host_game(player_name)
+
+
+### Helper Level Selection Function ####
 
 func _on_Char_Creation_pressed():
 	handle_level("Agency")
